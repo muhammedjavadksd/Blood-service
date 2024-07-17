fda7c5878f5f663caa24917b7b1c4d78383b7551
muhammedjavadksd <javadjazz899@gmail>
2024-07-17 16:00:29 +0530

    Enum for status code

--- src/Util/Types/Enum.ts
+++ src/Util/Types/Enum.ts
@@ -45,4 +45,15 @@ enum LocatedAt {
 }
 
 
-export { BloodGroup, BloodStatus, Relationship, LocatedAt }
+enum StatusCode {
+    OK = 200,
+    CREATED = 201,
+    UNAUTHORIZED = 401,
+    BAD_REQUESR = 400,
+    NOT_FOUND = 404,
+    SERVER_ERROR = 500,
+    FORBIDDEN = 403,
+    CONFLICT = 409,
+}
+
+export { BloodGroup, BloodStatus, Relationship, LocatedAt, StatusCode }
