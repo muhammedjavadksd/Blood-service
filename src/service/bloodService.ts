76eedbde0499f1781bbe8148a9bb200be68ca1b3
muhammedjavadksd <javadjazz899@gmail>
2024-07-22 18:38:15 +0530

    Blood service created

--- src/service/bloodService.ts
+++ src/service/bloodService.ts
@@ -2,27 +2,33 @@ import mongoose from "mongoose";
 import { BloodGroup, BloodStatus, LocatedAt, Relationship, StatusCode } from "../Util/Types/Enum";
 import { HelperFunctionResponse } from "../Util/Types/Interface/UtilInterface";
 import { mongoObjectId } from "../Util/Types/Types";
-import BloodRepo from "../repo/bloodRepo";
+import BloodRepo from "../repo/bloodReqRepo";
 import UtilHelper from "../Util/Helpers/UtilHelpers";
 import IBloodRequirement from "../Util/Types/Interface/ModelInterface";
+import BloodDonorRepo from "../repo/bloodDonorRepo";
 
 interface IBloodService {
     createBloodRequirement(patientName: string, unit: number, neededAt: Date, status: BloodStatus, user_id: mongoObjectId, profile_id: string, blood_group: BloodGroup, relationship: Relationship, locatedAt: LocatedAt, address: string, phoneNumber: number): Promise<HelperFunctionResponse>
+    createBloodId(blood_group: BloodGroup, unit: number): Promise<string>
 }
 
 class BloodService implements IBloodService {
 
-    private readonly bloodRepo: BloodRepo;
+    private readonly bloodReqRepo: BloodRepo;
+    private readonly bloodDonorRepo: BloodDonorRepo;
     private readonly utilHelper: UtilHelper;
 
     constructor() {
-        this.bloodRepo = new BloodRepo();
+        this.bloodReqRepo = new BloodRepo();
+        this.bloodDonorRepo = new BloodDonorRepo();
         this.utilHelper = new UtilHelper();
     }
 
+
+
     async createBloodId(blood_group: BloodGroup, unit: number): Promise<string> {
         let blood_id: string = blood_group + unit + this.utilHelper.createRandomText(5);
-        let isUserExist: IBloodRequirement | null = await this.bloodRepo.findBloodRequirementByBloodId(blood_id);
+        let isUserExist: IBloodRequirement | null = await this.bloodReqRepo.findBloodRequirementByBloodId(blood_id);
         while (isUserExist) {
             blood_id = blood_group + unit + this.utilHelper.createRandomText(5);
         }
@@ -32,7 +38,7 @@ class BloodService implements IBloodService {
 
     async createBloodRequirement(patientName: string, unit: number, neededAt: Date, status: BloodStatus, user_id: mongoObjectId, profile_id: string, blood_group: BloodGroup, relationship: Relationship, locatedAt: LocatedAt, address: string, phoneNumber: number): Promise<HelperFunctionResponse> {
         const blood_id: string = await this.createBloodId(blood_group, unit)
-        const createdBloodRequest: mongoObjectId | null = await this.bloodRepo.createBloodRequirement(blood_id, patientName, unit, neededAt, status, user_id, profile_id, blood_group, relationship, locatedAt, address, phoneNumber)
+        const createdBloodRequest: mongoObjectId | null = await this.bloodReqRepo.createBloodRequirement(blood_id, patientName, unit, neededAt, status, user_id, profile_id, blood_group, relationship, locatedAt, address, phoneNumber)
         if (createdBloodRequest) {
             return {
                 msg: "Blood requirement created success",
@@ -50,6 +56,10 @@ class BloodService implements IBloodService {
                 statusCode: StatusCode.SERVER_ERROR,
             }
         }
+    }
+
+    async bloodDonation(user_id, profile_id, donation_id) {
+
     }
 
 }
