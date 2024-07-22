c16899217fd95585b18e856a17bef8588e9ba0d5
muhammedjavadksd <javadjazz899@gmail>
2024-07-22 18:38:02 +0530

    admin, user, organization base router

--- src/router/organizationRouter.ts
+++ src/router/organizationRouter.ts
@@ -1,6 +1,19 @@
 import express, { Router } from 'express';
+import AuthMiddleware from '../middleware/authMiddelware';
+import UserController from '../controller/userController';
 
 const organizationRouter: Router = express.Router()
+const authMiddleware = new AuthMiddleware();
+const userController = new UserController();
 
+
+organizationRouter.get("/find_nearby", authMiddleware.isValidUser, userController.findNearBy)
+organizationRouter.get("/blood_availability", authMiddleware.isValidUser, userController.bloodAvailability)
+
+organizationRouter.post("/create", authMiddleware.isValidUser, userController.createBloodDonation)
+organizationRouter.post("/blood_request", authMiddleware.isValidUser, userController.blood_request)
+organizationRouter.post("/blood_donate/:donation_id", authMiddleware.isValidUser, userController.blood_donate)
+
+organizationRouter.patch("/close_request", authMiddleware.isValidUser, userController.closeRequest)
 
 export default organizationRouter
--- src/router/userRouter.ts
+++ src/router/userRouter.ts
@@ -6,7 +6,14 @@ const userRouter: Router = express.Router()
 const authMiddleware = new AuthMiddleware();
 const userController = new UserController();
 
+userRouter.get("/find_nearby", authMiddleware.isValidUser, userController.findNearBy)
+userRouter.get("/blood_availability", authMiddleware.isValidUser, userController.bloodAvailability)
+
+userRouter.post("/create", authMiddleware.isValidUser, userController.createBloodDonation)
 userRouter.post("/blood_request", authMiddleware.isValidUser, userController.blood_request)
+userRouter.post("/blood_donate/:donation_id", authMiddleware.isValidUser, userController.blood_donate)
+
+userRouter.patch("/close_request", authMiddleware.isValidUser, userController.closeRequest)
 
 
 export default userRouter
