8957aa199f0d0262b5500c8aef838529339369fc
muhammedjavadksd <javadjazz899@gmail>
2024-07-25 09:37:36 +0530

    Server js body reciver

--- server.ts
+++ server.ts
@@ -1,23 +1,31 @@
 
-import express, { Express } from 'express';
-import * as env from 'dotenv';
+import express, { Express, NextFunction, Request, Response } from 'express';
+import env from 'dotenv';
 import userRouter from './src/router/userRouter';
+// console.log("Worked1");
 import adminRouter from './src/router/adminRouter';
 import organizationRouter from './src/router/organizationRouter';
 import mongoDbConnection from './src/db/connection';
+import logger from 'morgan';
 
-env.config({ path: "./env" })
+env.config({ path: "./.env" })
 mongoDbConnection()
 
 const app: Express = express();
 const PORT: number = parseInt(process.env.PORT || '', 10) || 7007;
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(logger("combined"))
 
 app.use("/", userRouter)
 app.use("/admin", adminRouter)
 app.use("/organization", organizationRouter)
 
 
-app.use((err, req, res, next) => {
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.log(err);
     console.log("Error occured");
 })
 
